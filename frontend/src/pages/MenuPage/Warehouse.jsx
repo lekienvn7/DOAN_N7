500766b7711e34645bfdac37e344057c6ac74efd
lekienvn7 <ltrgkien68.68@gmail.com>
2025-10-29 10:57:43 +0700

    sua code

--- frontend/src/pages/MenuPage/Warehouse.jsx
+++ frontend/src/pages/MenuPage/Warehouse.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom"; // ⬅️ Thêm dòng này
 import RepoMenu from "@/components/RepoPage/RepoMenu";
 import { motion } from "framer-motion";
+import UtilityBox from "@/components/RepoPage/UtilityBox.jsx";
 
 const Warehouse = () => {
   return (
@@ -9,6 +10,7 @@ const Warehouse = () => {
       {/* Thanh menu cố định */}
       <motion.div className="sticky h-[120px] top-0 w-screen p-[20px] flex flex-row items-center justify-center bg-[#2c2c2e] shadow-[0_15px_25px_rgba(0,0,0,0),_0_10px_15px_rgba(0,0,0,0.4)] z-50">
         <RepoMenu />
+        <UtilityBox />
       </motion.div>
 
       {/* Vùng hiển thị trang con */}
